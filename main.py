eba61c684736826b62c87481994c1abd25bb3edf
Symbiomatrix <s.ymbiomatrix@gmail.com>
2019-03-22 03:36:57 +0200

    Initial code.

---
+++ main.py
@@ -0,0 +1,26 @@
+# -*- coding: cp1255 -*-
+#!! -*- coding: utf-8 -*-
+"""
+Created on 22/03/19
+
+@author: Symbiomatrix
+
+Todo:
+
+
+Features:
+
+
+Future:
+ 
+
+Notes:
+
+Bugs:
+
+Version log:
+22/03/19 V0.1 General structure initiated. 
+22/03/19 V0.0 New.
+
+"""
+
